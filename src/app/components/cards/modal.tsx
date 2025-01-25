d0fe63b9dc04000d58255ce139f2efe204be8f1f
iyiolakeni <iyiolakeni@gmail.com>
2025-01-25 23:09:21 +0100

    Updated Modal Setting

--- src/app/components/cards/modal.tsx
+++ src/app/components/cards/modal.tsx
@@ -143,15 +143,16 @@ const handleUpdate = () => {
           <div className="flex justify-end space-x-2 mt-4">
             <button 
               onClick={onClose}
-              className="px-4 py-2 bg-gray-200 text-gray-800 rounded-md hover:bg-gray-300"
+              className="px-4 py-2 bg-white text-blue-950 font-bold border border-blue-950 rounded-md"
             >
-              Cancel
+              cancel
             </button>
             <button 
               onClick={handleUpdate}
-              className="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+              className="px-4 py-2 bg-blue-950 font-bold text-white rounded-md hover:bg-indigo-700 flex items-center gap-2 group"
             >
-              Update
+              save
+              <span className="group-hover:translate-x-1 transition-transform"> →</span>
             </button>
           </div>
         </div>
